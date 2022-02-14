0ba0b65a72c2ab836569e0d6ccdc67b33503f0ae
alex <alex.song365@gmail.com>
2022-02-14 20:52:18 +0800

    01

---
+++ src/HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+//方法定义不能嵌套
+    public static void main(String[] args) {
+
+
+    }
+
+    public static void farmer(String[] args) {
+        System.out.println("farmer");
+    }
+
+    public static void seller(String[] args) {
+        System.out.println("seller");
+    }
+
+    public static void cook(String[] args) {
+        System.out.println("cook");
+    }
+
+    public static void me(String[] args) {
+        System.out.println("me");
+    }
+}
+
